1f9141deade601f440702a3a94500fe130f53e61
Anastasia-98Moiseeva <nepro100nastya@yandex.ru>
2020-10-20 20:50:08 +0300

    add task2
    task2

---
+++ task-2/untitled/src/com/company/Graph.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static sun.swing.MenuItemLayoutHelper.max;
+
+public class Graph {
+    Node root;
+
+    Graph(Node root){
+        this.root = root;
+    }
+
+    public void dfs(Node node, List<Integer> maxOnLevel, int curLevel){
+        if (maxOnLevel.size() <= curLevel){
+            maxOnLevel.add(node.key);
+        } else {
+            maxOnLevel.set(curLevel, max(maxOnLevel.get(curLevel), node.key));
+        }
+        if (node.left != null) {
+            dfs(node.left, maxOnLevel, curLevel+1);
+        }
+        if (node.right != null) {
+            dfs(node.right, maxOnLevel, curLevel+1);
+        }
+    }
+
+    public List<Integer> findMax(){
+        int curLevel = 0;
+        List<Integer> maxOnLevel = new ArrayList<Integer>();
+        dfs(this.root, maxOnLevel, curLevel);
+        return maxOnLevel;
+    }
+
+}
